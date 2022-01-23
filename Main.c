99872da2ef2b1ee89402540cdbfb999bc7dc8238
MicMuir <nicholas.muir2797@gmail.com>
2022-01-23 21:10:46 +0200

    Chromosome and Gene creation

--- Main.c
+++ Main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "MazeGeneration.c"
 #define Number_Of_Mazes 10
+#define walls 150
 
-
+struct Gene *Genes[Number_Of_Mazes];
 int **Mazes[Number_Of_Mazes];
 
 int main(){
     for(int Maze_Count=0;Maze_Count<Number_Of_Mazes;Maze_Count++){
-        Mazes[Maze_Count] = Generate_Random_Array();
+        
+        Mazes[Maze_Count] = Generate_Random_Maze();
+        //printf("here \n");
+        Genes[Maze_Count] = Create_Gene_From_Maze(Mazes[Maze_Count],walls);
+        Print_Maze(Mazes[Maze_Count]);
+        Print_Gene(Genes[Maze_Count],walls);
     }
-    Print_Maze(Mazes[0]);
+    
+    
+    
     return 0;
  }
--- MazeGeneration.c
+++ MazeGeneration.c
@@ -3,27 +3,61 @@
 
 #define size 20
  
-int** Generate_Random_Array(){
-    int **Maze;
+struct Gene{
+    int x,y;
+};
 
+int** Generate_Random_Maze(){
+    int **Maze;
     Maze = malloc(sizeof(int*)*size);
     for(int width_of_Maze=0;width_of_Maze<size;width_of_Maze++){
         Maze[width_of_Maze] = malloc(sizeof(int*)*size);
     }
-
     for(int width = 0 ; width<size ; width++){
         for(int height = 0;height<size;height++){
-            Maze[width][height] = rand() % 2;
+            int temp = rand() % 2;
+            Maze[height][width] = temp;
+            
         }
     }
+
     return(Maze);
 }
 
-void Print_Maze(int **Maze ){
+struct Gene* Create_Gene_From_Maze(int **Maze,int walls){
+    
+    struct Gene *Chromosome = malloc(walls * sizeof(struct Gene * ));
+    for(int i =0;i<walls;i++){
+        Chromosome[i].x = malloc(sizeof(int));
+        Chromosome[i].y = malloc(sizeof(int));
+    }
+    int GeneCounter = 0;
+    for(int width = 0 ; width<size ;width++){
+        for(int height = 0;height<size;height++){
+            if(Maze[height][width] == 1){
+                Chromosome[GeneCounter].x = width;
+                Chromosome[GeneCounter].y = height;
+                GeneCounter++;
+                }
+            }
+        }
+    
+    return(Chromosome);
+}
+
+void Print_Maze(int **Maze){
     for(int width = 0 ; width<size ; width++){
         for(int height = 0;height<size;height++){
             printf("%d",Maze[width][height]);
         }
+        printf("\n");
+    }
+}
+
+void Print_Gene(struct Gene *gene,int walls){
+    
+    for(int length_of_Gene = 0 ; length_of_Gene<walls ; length_of_Gene++){
+        printf("%d , %d",gene[length_of_Gene].x , gene[length_of_Gene].y);
         printf("\n");
     }
 }
